929ea867c58509136066d6e57bfb7857cc438b8f
Jishu Sardar <73737848+jishusardar@users.noreply.github.com>
2024-05-16 00:03:45 +0530

    Update hero.jsx

--- app/_component/hero.jsx
+++ app/_component/hero.jsx
@@ -6,45 +6,37 @@ import { useUser } from '@clerk/nextjs'
 function Hero() {
   const {user,isSignedIn}=useUser();
   return (
-    <section>
-      <div className="bg-gray-50 flex items-center flex-col">
-      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex">
-        <div className="mx-auto max-w-3xl text-center">
-          <h1
-            className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
-          >
-            Get Ready For Grady
+<section className="bg-gray-50">
+  <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+    <div className="mx-auto max-w-xl text-center">
+      <h1 className="text-3xl font-extrabold sm:text-5xl">
+      Get Ready For Grady.
+        <strong className="font-extrabold text-sky-700 sm:block"> The Grade Analytical Tool. </strong>
+      </h1>
 
-            <span className="sm:block"> :The Grade Analytical Tool</span>
-          </h1>
+      <p className="mt-4 sm:text-xl/relaxed">
+      Grady Is A Platform Capable of Multi User Calculation & Data Visualization
+      </p>
 
-          <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
-            Grady Is A Platform Capable of Multi User Calculation & Data Visualization
-          </p>
-
-          <div className="mt-8 flex flex-wrap justify-center gap-4">
-            {isSignedIn ?
-              <a
-                className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-purple-600 hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-                href="./dash"
-              >
-                Dashboard
-              </a> :
-              <a
-                className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-purple-600 hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-                href="./sign-in"
-              >
-                Sign In
-              </a>
-            }
-          </div>
-        </div>
+      <div className="mt-8 flex flex-wrap justify-center gap-4">
+      {isSignedIn ?
+        <a
+          className="block w-full rounded bg-sky-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-sky-700 focus:outline-none focus:ring active:bg-sky-500 sm:w-auto"
+          href="./dash"
+        >
+          Dashboard
+        </a>:
+        <a
+        className="block w-full rounded bg-sky-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-sky-700 focus:outline-none focus:ring active:bg-sky-500 sm:w-auto"
+        href="./sign-in"
+      >
+        SignIn
+      </a>
+        }
       </div>
-      {isSignedIn?
-      <Image src='/dashboard.png' alt="Seems Like You Haven't Signed Up yet" width={1000} height={700} className='-mt-9 rounded-xl border-2' />:
-      <Image src='/dashboard.png' alt="Seems Like You Haven't Signed Up yet" width={1000} height={700} className='-mt-9 rounded-xl border-2' />}
-      </div>
-    </section>
+    </div>
+  </div>
+</section>
   )
 }
 
