f477ac56bf92edf064aa68a47ea43bc93e4b7cc4
Jishu Sardar <73737848+jishusardar@users.noreply.github.com>
2024-05-31 23:01:21 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
+"use client"
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { redirect } from 'next/navigation'
 import Header from './_component/header'
 import Hero from './_component/hero'
+import React, { useEffect, useState } from 'react'
+import { useUser } from '@clerk/nextjs'
 function page() {
+  const { user }=useUser();
+  useEffect(() => {
+    if (user) {
+      redirect('/dash')
+    }
+  }, [user]);
   return (
     <div>
       <Header/>
